df16219318a68012a1dbb956bc24db15f318dca6
bleedrnge <bleedrnge@gmail.com>
2015-09-10 21:41:49 -0400

    adding light and door controllers

---
+++ gpio/door.c
@@ -0,0 +1,18 @@
+#include <wiringPi.h>
+
+//pin to use for lights
+#define PIN 18
+
+int main (void)
+{
+	wiringPiSetup () ;
+	pinMode (PIN, OUTPUT) ;
+	
+	//enable relay, pause, disable relay, pause
+	digitalWrite (PIN, HIGH);
+	delay (500) ;
+	
+	digitalWrite (PIN, LOW);
+	delay (500) ;
+	return 0 ;
+}
